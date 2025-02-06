18d78d8f4138cce7353c64f53475ef25570360ef
Diego-Poblete <dpobletem@fen.uchile.cl>
2025-02-06 12:49:42 -0300

    upgrade11

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -28,71 +28,73 @@ document.addEventListener("DOMContentLoaded", (event) => {
   });
 });
 
-const messages = [
-  `<div>
-    <p>Hey there 👋</p>
-    <br>
-    <p>I'm here to help you with any questions or concerns you may have.😊</p>
-  </div>`,
+
+
+// const messages = [
+//   `<div>
+//     <p>Hey there 👋</p>
+//     <br>
+//     <p>I'm here to help you with any questions or concerns you may have.😊</p>
+//   </div>`,
   
-  `<div>
-    <p>¡Hola! tienes alguna consulta?</p>
-    <br>
-    <p>¿Cómo puedo ayudarte? 😊</p>
-  </div>`,
+//   `<div>
+//     <p>¡Hola! tienes alguna consulta?</p>
+//     <br>
+//     <p>¿Cómo puedo ayudarte? 😊</p>
+//   </div>`,
 
-  `<div>
-    <p>مرحبًا 👋</p>
-    <br>
-    <p>أنا هنا للمساعدة في أي أسئلة أو استفسارات قد تكون لديك.😊</p>
-  </div>`,
+//   `<div>
+//     <p>مرحبًا 👋</p>
+//     <br>
+//     <p>أنا هنا للمساعدة في أي أسئلة أو استفسارات قد تكون لديك.😊</p>
+//   </div>`,
 
-  `<div>
-    <p>안녕하세요 👋</p>
-    <br>
-    <p>궁금한 점이나 걱정되는 부분이 있으면 도와드리겠습니다.😊</p>
-  </div>`,
-];
+//   `<div>
+//     <p>안녕하세요 👋</p>
+//     <br>
+//     <p>궁금한 점이나 걱정되는 부분이 있으면 도와드리겠습니다.😊</p>
+//   </div>`,
+// ];
 
-let currentMessageIndex = 0;
+// let currentMessageIndex = 0;
 
-// Función para actualizar el mensaje
-function updateMessage() {
-  const messageElement = document.querySelector(
-    ".ChatLayout__MessageContent-sc-ub8sor-7 .ChatLayout__Text-sc-ub8sor-8"
-  );
+// // Función para actualizar el mensaje
+// function updateMessage() {
+//   const messageElement = document.querySelector(
+//     ".ChatLayout__MessageContent-sc-ub8sor-7 .ChatLayout__Text-sc-ub8sor-8"
+//   );
 
-  // Verificar si el mensaje ya existe
-  if (messageElement) {
-    const currentText = messageElement.innerHTML.trim();
+//   // Verificar si el mensaje ya existe
+//   if (messageElement) {
+//     const currentText = messageElement.innerHTML.trim();
 
-    // Solo actualizamos si el mensaje actual es diferente
-    if (currentText !== messages[currentMessageIndex]) {
-      messageElement.innerHTML = messages[currentMessageIndex];
-      currentMessageIndex = (currentMessageIndex + 1) % messages.length; // Cambia al siguiente mensaje
-    }
-  } else {
-    // Si no existe el mensaje, lo creamos
-    const chatContainer = document.querySelector(".ChatLayout__MessageContainer-sc-ub8sor-4");
-    if (chatContainer) {
-      const newMessageElement = document.createElement("div");
-      newMessageElement.classList.add("ChatLayout__MessageContent-sc-ub8sor-7", "qVqIO");
-      newMessageElement.innerHTML = `<div class="ChatLayout__MessageGroup-sc-ub8sor-10 bAgvJV">
-                                       <div class="ChatLayout__Text-sc-ub8sor-8 izDmXy">
-                                         ${messages[currentMessageIndex]}
-                                       </div>
-                                     </div>`;
-      chatContainer.appendChild(newMessageElement);
-      currentMessageIndex = (currentMessageIndex + 1) % messages.length; // Cambia al siguiente mensaje
-    }
-  }
-}
+//     // Solo actualizamos si el mensaje actual es diferente
+//     if (currentText !== messages[currentMessageIndex]) {
+//       messageElement.innerHTML = messages[currentMessageIndex];
+//       currentMessageIndex = (currentMessageIndex + 1) % messages.length; // Cambia al siguiente mensaje
+//     }
+//   } else {
+//     // Si no existe el mensaje, lo creamos
+//     const chatContainer = document.querySelector(".ChatLayout__MessageContainer-sc-ub8sor-4");
+//     if (chatContainer) {
+//       const newMessageElement = document.createElement("div");
+//       newMessageElement.classList.add("ChatLayout__MessageContent-sc-ub8sor-7", "qVqIO");
+//       newMessageElement.innerHTML = `<div class="ChatLayout__MessageGroup-sc-ub8sor-10 bAgvJV">
+//                                        <div class="ChatLayout__Text-sc-ub8sor-8 izDmXy">
+//                                          ${messages[currentMessageIndex]}
+//                                        </div>
+//                                      </div>`;
+//       chatContainer.appendChild(newMessageElement);
+//       currentMessageIndex = (currentMessageIndex + 1) % messages.length; // Cambia al siguiente mensaje
+//     }
+//   }
+// }
 
-// Cambia el mensaje cada 4 segundos
-setInterval(updateMessage, 4000);
+// // Cambia el mensaje cada 4 segundos
+// setInterval(updateMessage, 4000);
 
-// Inicializa con el primer mensaje
-updateMessage();
+// // Inicializa con el primer mensaje
+// updateMessage();
 
 
 //IntersectionObserver
